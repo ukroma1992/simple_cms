e0a46581aa9113f5f3506fba6a011b232f4c375a
ukroma1992 <ukroma1992@gmail.com>
2019-02-13 16:45:07 +0200

    Added scopes for subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ApplicationRecord
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
