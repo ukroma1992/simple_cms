08837627c0467e86f2e851aa6abe39208b1f2374
ukroma1992 <ukroma1992@gmail.com>
2019-02-14 14:25:39 +0200

    Add rich associations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,6 @@ class AdminUser < ApplicationRecord
   # self.table_name = "admin_users"
 
   has_and_belongs_to_many :pages
+  has_many :section_edits
+  has_many :sections, through: :section_edits
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_111644) do
+ActiveRecord::Schema.define(version: 2019_02_14_114051) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "first_name", limit: 25
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2019_02_14_111644) do
     t.datetime "updated_at", null: false
     t.index ["permalink"], name: "index_pages_on_permalink"
     t.index ["subject_id"], name: "index_pages_on_subject_id"
+  end
+
+  create_table "section_edits", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "section_id"
+    t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_user_id", "section_id"], name: "index_section_edits_on_admin_user_id_and_section_id"
   end
 
   create_table "sections", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
